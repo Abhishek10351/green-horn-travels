3ce3a112170957b967a38875555ec5f890d19a5e
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-05-29 23:46:41 +0530

    added navbar

--- sites/home/Home.jsx
+++ sites/home/Home.jsx
@@ -1,5 +1,11 @@
 import PackageCard from "../../components/PackageCard/PackageCard";
 import data from "../../public/assets/data/packages.json";
+import Navbar from "../../components/Navbar/Navbar";
 export function Home() {
-    return <></>;
+    return( <>
+    <Navbar />
+    
+    
+    </>
+    );
 }
