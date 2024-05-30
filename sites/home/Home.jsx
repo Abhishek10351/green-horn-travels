4e44689b7fefbff841e8249ee9648b0b6a60dd2c
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-05-30 20:05:03 +0530

    Added Hero section

--- sites/home/Home.jsx
+++ sites/home/Home.jsx
@@ -1,9 +1,11 @@
 import PackageCard from "../../components/PackageCard/PackageCard";
 import data from "../../public/assets/data/packages.json";
 import Navbar from "../../components/Navbar/Navbar";
+import Hero from "../../components/Hero/Hero";
 export function Home() {
     return( <>
     <Navbar />
+    <Hero />
     
     
     </>
