81d2898b1400d66c4e8e171f612c3c0868be25b4
Abhishek Kashyap <abhik5567@gmail.com>
2024-06-07 19:20:51 +0530

    move about component

--- components/about/welcome/Welcome.jsx
+++ components/about/welcome/Welcome.jsx
@@ -6,10 +6,10 @@ export default function Welcome() {
             <h1 className="about-h1">About Us</h1>
             <p className="about-p">
                 Welcome to Green Horn Travels! At Green Horn Travels, we believe
-                that travel is not just about visiting new places; it's about
-                creating unforgettable experiences and cherished memories. With
-                a passion for exploration and a commitment to excellence, we
-                strive to provide our customers with the most immersive and
+                that travel is not just about visiting new places; it&apos;s
+                about creating unforgettable experiences and cherished memories.
+                With a passion for exploration and a commitment to excellence,
+                we strive to provide our customers with the most immersive and
                 enjoyable travel experiences in Northeast India. Founded with
                 the vision of showcasing the hidden gems and natural wonders of
                 Assam and its neighboring states, Green Horn Travels is your
--- sites/about/About.jsx
+++ sites/about/About.jsx
@@ -1,9 +1,22 @@
+import "./about.css";
+import Address from "../../components/about/address/Address";
+import Welcome from "../../components/about/welcome/Welcome";
+import DiscoverySection from "../../components/about/discovery/DiscoverySection";
+import Team from "../../components/about/team/Team";
+import Adventure from "../../components/about/adventure/Adventure";
 
-import AboutComponent from '../../components/about/About/About';
+import Footer from "../../components/globals/Footer/Footer";
 export default function About() {
     return (
         <>
-        <AboutComponent />
+            <main className="container">
+                <Welcome />
+                <DiscoverySection />
+                <Team />
+                <Adventure />
+                <Address />
+            </main>
+            <Footer />
         </>
-    )
+    );
 }
