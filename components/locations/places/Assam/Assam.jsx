811dd59e245668f154bf1740eb05673d8d94b907
Abhishek Kashyap <abhik5567@gmail.com>
2024-06-08 23:46:17 +0530

    fix places components

--- components/locations/places/Assam/Assam.jsx
+++ components/locations/places/Assam/Assam.jsx
@@ -1,43 +1,42 @@
 import Footer from "../../../globals/Footer/Footer";
 import "./Assam.css";
 import PlacesCardSection from "../../PlacesCardSection/PlacesCardSection";
-import placesData from "../../../../public/assets/data/packages.json"; // Assuming places.json is located in the src folder
+import placesData from "../../../../public/assets/data/packages.json";
 
-function Assam() {
-  // Filter placesData for locations in Assam
-  const assamPlaces = placesData.filter((place) => place.location === "Assam");
+export default function Assam() {
+    // Filter placesData for locations in Assam
+    const assamPlaces = placesData.filter(
+        (place) => place.location === "Assam"
+    );
 
-  return (
-    <>
-      <div className="assam-container">
-        <section>
-          <h1>Hello from Assam</h1>
-          <div className="intro-assam">
-            <p>
-              Land of One-Horned Rhinos and Serene Beauty. Lush green hills
-              shrouded in mist, the mighty Brahmaputra slicing through the
-              heartland, and the echoing calls of wildlife in Kaziranga National
-              Park - Assam is a tapestry woven with nature's grandeur. Here,
-              ancient temples whisper tales of bygone eras, while vibrant
-              festivals like Bihu showcase the warmth and color of Assamese
-              culture. Explore the majestic Kamakhya Temple, a revered
-              pilgrimage site, or lose yourself in the labyrinthine alleys of
-              the historic town of Majuli, the world's largest river island.
-              Assam's rich heritage is evident in its handloom textiles, with
-              vibrant silks and intricate designs waiting to be discovered.
-            </p>
-          </div>
-        </section>
-        {/* make section here */}
-        <section className="places-grid">
-          {/* Map over filtered Assam placesData and render PlacesCard */}
-          
-        <PlacesCardSection {...assamPlaces} />
-        </section>
-      </div>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <div className="assam-container">
+                <section>
+                    <h1>Hello from Assam</h1>
+                    <div className="intro-assam">
+                        <p>
+                            Land of One-Horned Rhinos and Serene Beauty. Lush
+                            green hills shrouded in mist, the mighty Brahmaputra
+                            slicing through the heartland, and the echoing calls
+                            of wildlife in Kaziranga National Park - Assam is a
+                            tapestry woven with nature's grandeur. Here, ancient
+                            temples whisper tales of bygone eras, while vibrant
+                            festivals like Bihu showcase the warmth and color of
+                            Assamese culture. Explore the majestic Kamakhya
+                            Temple, a revered pilgrimage site, or lose yourself
+                            in the labyrinthine alleys of the historic town of
+                            Majuli, the world's largest river island. Assam's
+                            rich heritage is evident in its handloom textiles,
+                            with vibrant silks and intricate designs waiting to
+                            be discovered.
+                        </p>
+                    </div>
+                </section>
+
+                <PlacesCardSection places={assamPlaces} />
+            </div>
+            <Footer />
+        </>
+    );
 }
-
-export default Assam;
--- components/locations/places/Meghalaya/Meghalaya.jsx
+++ components/locations/places/Meghalaya/Meghalaya.jsx
@@ -1,31 +1,39 @@
-import Footer from "../../../components/Footer/Footer";
-import './Meghalaya.css';
-import PlacesCard from '../../../components/PlacesCard/PlacesCard';
-import placesData from '../../../packages.json'; // Assuming places.json is located in the src folder
+import Footer from "../../../../components//globals/Footer/Footer";
+import "./Meghalaya.css";
+import PlacesCardSection from "../../PlacesCardSection/PlacesCardSection";
+import placesData from "../../../../public/assets/data/packages.json";
 
-function Meghalaya() {
-  // Filter placesData for locations in Meghalaya
-  const MeghalayaPlaces = placesData.filter(place => place.location === "Meghalaya");
+export default function Meghalaya() {
+    // Filter placesData for locations in Meghalaya
+    const MeghalayaPlaces = placesData.filter(
+        (place) => place.location === "Meghalaya"
+    );
 
-  return (
-    <>
-      <div className="Meghalaya-container">
-        <h1>Hello from Meghalaya</h1>
-        <div className="intro-Meghalaya">
-          <p>
-          The beautiful state of Meghalaya, located in northeastern India, is known for its breathtaking landscapes and rich cultural heritage. With its rolling hills, cascading waterfalls, and lush greenery, Meghalaya offers a paradise for nature lovers and adventure enthusiasts. The state is home to diverse indigenous communities, each with its unique traditions, music, and cuisine. Whether you're exploring the living root bridges in Cherrapunji, trekking through the picturesque valleys of Mawlynnong, or experiencing the vibrant festivals of Shillong, Meghalaya promises an unforgettable journey into the heart of nature and culture.
-          </p>
-        </div>
-        <div className="places-grid">
-        {/* Map over filtered Meghalaya placesData and render PlacesCard */}
-        {MeghalayaPlaces.map(place => (
-          <PlacesCard key={place.id} place={place} />
-        ))}
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <div className="Meghalaya-container">
+                <h1>Hello from Meghalaya</h1>
+                <div className="intro-Meghalaya">
+                    <p>
+                        The beautiful state of Meghalaya, located in
+                        northeastern India, is known for its breathtaking
+                        landscapes and rich cultural heritage. With its rolling
+                        hills, cascading waterfalls, and lush greenery,
+                        Meghalaya offers a paradise for nature lovers and
+                        adventure enthusiasts. The state is home to diverse
+                        indigenous communities, each with its unique traditions,
+                        music, and cuisine. Whether you're exploring the living
+                        root bridges in Cherrapunji, trekking through the
+                        picturesque valleys of Mawlynnong, or experiencing the
+                        vibrant festivals of Shillong, Meghalaya promises an
+                        unforgettable journey into the heart of nature and
+                        culture.
+                    </p>
+                </div>
+
+                <PlacesCardSection places={MeghalayaPlaces} />
+            </div>
+            <Footer />
+        </>
+    );
 }
-
-export default Meghalaya;
