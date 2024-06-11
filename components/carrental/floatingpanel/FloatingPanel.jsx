57716af4ec1de7fe0895e3886626e37e109acebe
Abhishek Kashyap <abhik5567@gmail.com>
2024-06-11 10:12:26 +0530

    fix floating panel icon

--- components/carrental/floatingpanel/FloatingPanel.jsx
+++ components/carrental/floatingpanel/FloatingPanel.jsx
@@ -1,14 +1,16 @@
-import './FloatingPanel.css';
+import "./FloatingPanel.css";
 
 export default function FloatingPanel() {
     return (
-      <section className="floating-panel">
-        <a href="https://wa.me/7002822793" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-whatsapp"></i>
-          <span>Book Now!</span>
-        </a>
-      </section>
+        <aside className="floating-panel">
+            <a
+                href="https://wa.me/7002822793"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <i className="fa fa-whatsapp"></i>
+                <span>Book Now!</span>
+            </a>
+        </aside>
     );
-  }
-  
-  
+}
