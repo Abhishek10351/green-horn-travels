850e7a9e0edc7dcf50d9dace709d97f37bcde4f7
Abhishek Kashyap <abhik5567@gmail.com>
2024-06-13 00:05:36 +0530

    add packages section

---
+++ components/location/PackagesCardSection/PackagesCard.jsx
@@ -0,0 +1,28 @@
+import "./Packages.css";
+export default function PackagesCard({ title, image, info, overlay }) {
+    const image_path = "/public/assets/package-images/";
+    const package_path = `/location/${title.toLowerCase()}`;
+    return (
+        <div className="ag-packages_item">
+            <a href={package_path} className="ag-packages-item_link">
+                <div
+                    className="ag-packages-item_bg"
+                    style={{
+                        backgroundImage: `url(${image_path}${image})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                    }}
+                ></div>
+                <div
+                    className="ag-packages-item_overlay"
+                    style={{
+                        backgroundColor: overlay,
+                    }}
+                ></div>
+                <div className="ag-packages-item_title">{title}</div>
+                <button className="package-card-button">Book</button>
+                <div className="ag-packages-item_info">{info}</div>
+            </a>
+        </div>
+    );
+}
