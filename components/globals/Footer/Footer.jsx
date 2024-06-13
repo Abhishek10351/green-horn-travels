b1e7314c8c6d08f71f42e7d2cc21cfc9b4f1a831
Abhishek Kashyap <abhik5567@gmail.com>
2024-06-13 21:06:33 +0530

    fix main footer grid

--- components/globals/Footer/Footer.jsx
+++ components/globals/Footer/Footer.jsx
@@ -6,6 +6,7 @@ export default function Footer() {
         <footer className="footer">
             <div className="footer-logo">
                 <Image
+                    className="footer-logo-img"
                     src="/public/assets/logo/white-bg.png"
                     alt=""
                     width="100"
@@ -14,39 +15,39 @@ export default function Footer() {
                 <p>Green Horn Travels</p>
             </div>
             <div className="footer-links">
-                <div className="quicklinks">
-                    <h3>Quick Links</h3>
-                    <ul>
-                        <li>
-                            <a href="/">Home</a>
-                        </li>
-                        <li>
-                            <a href="/about">About us</a>
-                        </li>
-                        <li>
-                            <a href="/locations">Packages</a>
-                        </li>
-                        <li>
-                            <a href="/rentals">Car Rentals</a>
-                        </li>
-                        <li>
-                            <a href="#">Contact Us</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div className="address">
-                    <h3>Address</h3>
-                    <address>
-                        Lichubagan, Hengrabari Guwahati, Assam <br />
-                        Pin Code - 781036
-                    </address>
-                </div>
+                <h3>Quick Links</h3>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/about">About us</a>
+                    </li>
+                    <li>
+                        <a href="/locations">Packages</a>
+                    </li>
+                    <li>
+                        <a href="/rentals">Car Rentals</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="footer-address">
+                <h3>Address</h3>
+                <address>
+                    Lichubagan, Hengrabari Guwahati, Assam <br />
+                    Pin Code - 781036
+                </address>
             </div>
 
             <div className="footer-social">
                 <div className="social-icons">
                     <h3>Connect with us</h3>
+                    <a href="tel:+917002822793">
+                        <i className="fa fa-phone"></i>
+                    </a>
                     <a href="#">
                         <i className="fa fa-facebook"></i>
                     </a>
@@ -64,7 +65,7 @@ export default function Footer() {
                     className="map-iframe"
                     title="office-location"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3581.3202272193926!2d91.8001233!3d26.153698499999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x375a59007cbafba1%3A0x806a75f100c1fe73!2sGreen%20Horn%20Travels%20-%20Travel%20Agency%20in%20Guwahati!5e0!3m2!1sen!2sin!4v1717577556663!5m2!1sen!2sin"
-                    width="100%"
+                    width="400"
                     height="250"
                     style={{ border: 0 }}
                     allowFullScreen=""
