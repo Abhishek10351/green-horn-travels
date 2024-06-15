8fc338fa0a3334abf6db2c48ab27cbcc7c656285
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-06-15 12:54:56 +0530

    added tawang img and updated the css with tagline

--- components/home/TopPackagesSection/TopPackagesSection.jsx
+++ components/home/TopPackagesSection/TopPackagesSection.jsx
@@ -2,36 +2,36 @@ import TopPackageCard from "./TopPackageCard";
 import "./TopPackagesSection.css";
 
 const topPackages = [
-    {
-        title: "Sivasagar",
-        description:
-            "Enjoy the enriching beauty of the historical town of Sivasagar.",
-        image: "sivsagar.jpg",
-    },
-    {
-        title: "Kaziranga",
-        description:
-            "Explore the majestic beauty and serenity of the national park.",
-        image: "kaziranga.jpg",
-    },
-    {
-        title: "Meghalaya",
-        description: "Adventure awaits in Meghalaya's landscapes.",
-        image: "meghalaya.jpg",
-    },
+  {
+    title: "Tawang",
+    description:
+      "Witness the timeless traditions and spiritual grandeur of Tawang.",
+    image: "Tawang.jpg",
+  },
+  {
+    title: "Kaziranga",
+    description:
+      "Explore the majestic beauty and serenity of the national park.",
+    image: "kaziranga.jpg",
+  },
+  {
+    title: "Meghalaya",
+    description: "Adventure awaits in Meghalaya's landscapes.",
+    image: "meghalaya.jpg",
+  },
 ];
 
 function TopPackagesSection() {
-    return (
-        <section className="top-packages-section">
-            <h2>Top Travel Packages</h2>
-            <div className="packages-container">
-                {topPackages.map((pkg, index) => (
-                    <TopPackageCard key={index} {...pkg} />
-                ))}
-            </div>
-        </section>
-    );
+  return (
+    <section className="top-packages-section">
+      <h2>Top Travel Packages</h2>
+      <div className="packages-container">
+        {topPackages.map((pkg, index) => (
+          <TopPackageCard key={index} {...pkg} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default TopPackagesSection;
