b8df5ed717252496d40d3049af1ecdd87034a30f
Abhishek Kashyap <abhik5567@gmail.com>
2024-06-16 09:23:04 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // add dir for static files
-    assetPrefix: '/public/',
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-                port: '',
-            },
-        ],
-    },
+    assetPrefix: '/public',
 };
 
 export default nextConfig;
