4afac4070c3dc624207788a470515d6476da4cd5
Abhishek Kashyap <abhik5567@gmail.com>
2024-06-16 20:53:14 +0530

    add carousel in home page

--- sites/home/Home.jsx
+++ sites/home/Home.jsx
@@ -2,6 +2,7 @@ import Navbar from "../../components/globals/Navbar/Navbar";
 import Hero from "../../components/home/Hero/Hero";
 import AboutUs from "../../components/home/About us/AboutUs";
 import TopPackagesSection from "../../components/home/TopPackagesSection/TopPackagesSection";
+import Carousel from "../../components/home/carousel/Carousel";
 import Footer from "../../components/globals/Footer/Footer";
 export function Home() {
     return (
@@ -10,6 +11,7 @@ export function Home() {
             <Hero />
             <AboutUs />
             <TopPackagesSection />
+            <Carousel />
             <Footer />
         </>
     );
