4f6ea4456de9a6b15f85281c04eae4d6a737f34b
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-06-18 20:37:32 +0530

    booking links fix

--- components/carrental/vehiclecard/VehicleCard.jsx
+++ components/carrental/vehiclecard/VehicleCard.jsx
@@ -4,26 +4,28 @@ import { useInView } from "react-intersection-observer";
 import "./VehicleCard.css";
 
 export default function VehicleCard({ ...props }) {
-    // const { ref, inView } = useInView();
+  // const { ref, inView } = useInView();
 
-    function handleBookNow(vehicleName) {
-        alert(`Book Now: ${vehicleName}`);
-    }
-    // <section ref={ref} className={`vehicle-card ${inView ? 'fade-in' : ''}`}>
+  function handleBookNow(vehicleName) {
+    alert(`Book Now: ${vehicleName}`);
+  }
+  // <section ref={ref} className={`vehicle-card ${inView ? 'fade-in' : ''}`}>
 
-    return (
-        <section className={`vehicle-card`}>
-            <Image
-                src={`/assets/cars/${props.img}`}
-                alt={props.name}
-                width={300}
-                height={200}
-                className="vehicle-image"
-            />
-            <h2>{props.name}</h2>
-            <p>{props.description}</p>
-            <button className="vehicle-button">Book Now</button>
-        </section>
-    );
-    // <button onClick={() => handleBookNow(props.name)}>Book Now</button>;
+  return (
+    <section className={`vehicle-card`}>
+      <Image
+        src={`/assets/cars/${props.img}`}
+        alt={props.name}
+        width={300}
+        height={200}
+        className="vehicle-image"
+      />
+      <h2>{props.name}</h2>
+      <p>{props.description}</p>
+      <a href="https://wa.me/7002822793" target="_blank">
+        <button className="vehicle-button">Book Now</button>
+      </a>
+    </section>
+  );
+  // <button onClick={() => handleBookNow(props.name)}>Book Now</button>;
 }
