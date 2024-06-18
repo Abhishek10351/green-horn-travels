1cc7f5386d48278e93892730a60c7d86c01704cc
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-06-18 18:03:41 +0530

    added navbar in about page

--- sites/about/About.jsx
+++ sites/about/About.jsx
@@ -4,19 +4,20 @@ import Welcome from "../../components/about/welcome/Welcome";
 import DiscoverySection from "../../components/about/discovery/DiscoverySection";
 import Team from "../../components/about/team/Team";
 import Adventure from "../../components/about/adventure/Adventure";
-
+import Navbar from "../../components/globals/Navbar/Navbar";
 import Footer from "../../components/globals/Footer/Footer";
 export default function About() {
-    return (
-        <>
-            <main className="container">
-                <Welcome />
-                <DiscoverySection />
-                <Team />
-                <Adventure />
-                <Address />
-            </main>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <main className="container">
+        <Welcome />
+        <DiscoverySection />
+        <Team />
+        <Adventure />
+        <Address />
+      </main>
+      <Footer />
+    </>
+  );
 }
