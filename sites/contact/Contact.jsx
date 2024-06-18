998482d0906a9784afec6748be9eb7cb09b160d3
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-06-18 19:43:23 +0530

    added navbar

--- sites/contact/Contact.jsx
+++ sites/contact/Contact.jsx
@@ -1,10 +1,11 @@
 import SocialMedia from "../../components/contact/SocialMedia/SocialMedia";
 import ContactImage from "../../components/contact/ContactImage/ContactImage";
+import Navbar from "../../components/globals/Navbar/Navbar";
 import Footer from "../../components/globals/Footer/Footer";
 import "./Contact.css";
 export default function Contact() {
   return (
-    <>
+    <> <Navbar />
       <div className="contact-container">
         <SocialMedia />
         <ContactImage />
