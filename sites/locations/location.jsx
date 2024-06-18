1419b899b3be36aeb8502a963249860406267e86
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-06-18 18:20:42 +0530

    added navbar and footer in packages page

--- sites/locations/location.jsx
+++ sites/locations/location.jsx
@@ -1,9 +1,15 @@
 import PackagesSection from "../../components/location/PackagesCardSection/PackageCardSection";
-
+import Navbar from "../../components/globals/Navbar/Navbar";
+import Footer from "../../components/globals/Footer/Footer";
+import "./location.css";
 export default function Location() {
-    return (
-        <div>
-            <PackagesSection />
-        </div>
-    );
+  return (
+    <>
+      <Navbar />
+      <div>
+        <PackagesSection />
+      </div>
+      <Footer />
+    </>
+  );
 }
