d81d98f9d8e8add0af0032a1599915d8f73d42c5
Abhishek Kashyap <abhik5567@gmail.com>
2024-06-19 10:23:22 +0530

    add navbar to location

--- sites/locations/location.jsx
+++ sites/locations/location.jsx
@@ -1,8 +1,10 @@
 import PackagesSection from "../../components/location/PackagesCardSection/PackageCardSection";
+import Navbar from "../../components/globals/Navbar/Navbar";
 import Footer from "../../components/globals/Footer/Footer";
 export default function Location() {
     return (
         <>
+            <Navbar />
             <main>
                 <PackagesSection />
             </main>
