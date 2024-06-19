e465a634b92e32d5151414f65a5a93f2317401a1
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-06-19 20:32:03 +0530

    added title in package page

---
+++ components/location/PackagesCardSection/PackageTitle.jsx
@@ -0,0 +1,8 @@
+import "./PackageTitle.css";
+export default function PackageTitle() {
+  return (
+    <section className="package-title">
+      <h1>Our Tour Packages</h1>
+    </section>
+  );
+}
--- sites/locations/location.jsx
+++ sites/locations/location.jsx
@@ -1,10 +1,12 @@
 import PackagesSection from "../../components/location/PackagesCardSection/PackageCardSection";
 import Navbar from "../../components/globals/Navbar/Navbar";
 import Footer from "../../components/globals/Footer/Footer";
+import PackageTitle from "../../components/location/PackagesCardSection/PackageTitle";
 export default function Location() {
     return (
         <>
             <Navbar />
+            <PackageTitle />
             <main>
                 <PackagesSection />
             </main>
