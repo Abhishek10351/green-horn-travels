de674b81bbc7274ac7307bba7ea451d4d26c19b8
Abhishek Kashyap <abhik5567@gmail.com>
2024-06-20 14:06:05 +0530

    add custom whatsapp links

--- components/carrental/vehiclecard/VehicleCard.jsx
+++ components/carrental/vehiclecard/VehicleCard.jsx
@@ -3,20 +3,22 @@ import "./VehicleCard.css";
 
 export default function VehicleCard({ ...props }) {
 
-  return (
-    <div className={`vehicle-card`}>
-      <Image
-        src={`/assets/cars/${props.img}`}
-        alt={props.name}
-        width={300}
-        height={200}
-        className="vehicle-image"
-      />
-      <h2>{props.name}</h2>
-      <p>{props.description}</p>
-      <a href="https://wa.me/7002822793" target="_blank">
-        <button className="vehicle-button">Book Now</button>
-      </a>
-    </div>
-  );
+  const url_text = encodeURIComponent(`Hello, I am interested in booking a ${props.name}. Can you please provide me with more information?`);
+  const url = `https://wa.me/7002822793?text=${url_text}`;
+    return (
+        <div className={`vehicle-card`}>
+            <Image
+                src={`/assets/cars/${props.img}`}
+                alt={props.name}
+                width={300}
+                height={200}
+                className="vehicle-image"
+            />
+            <h2>{props.name}</h2>
+            <p>{props.description}</p>
+            <a href={url} target="_blank">
+                <button className="vehicle-button">Book Now</button>
+            </a>
+        </div>
+    );
 }
--- components/locations/PlacesCardSection/PlacesCard.jsx
+++ components/locations/PlacesCardSection/PlacesCard.jsx
@@ -1,24 +1,28 @@
 import "./PlacesCard.css";
 import Image from "next/image";
 export default function PlacesCard({ ...place }) {
-  return (
-    <article className="card">
-      <Image
-        className="card-background"
-        src={`/assets/locations/${place.img}`}
-        alt={`Photo of ${place.placeName}`}
-        width={400}
-        height={300}
-      />
-      <div className="card-content">
-        <div className="card-content-container">
-          <h2 className="card-title">{place.placeName}</h2>
-          <p className="card-description">{place.info}</p>
-        </div>
-        <a href="https://wa.me/7002822793" target="_blank">
-          <button className="card-button">Book Now</button>
-        </a>
-      </div>
-    </article>
-  );
+    const url_text = encodeURIComponent(
+        `I am interested in booking a trip to ${place.placeName}. Can you please provide me with more information?`
+    );
+    const url = `https://wa.me/7002822793?text=${url_text}`;
+    return (
+        <article className="card">
+            <Image
+                className="card-background"
+                src={`/assets/locations/${place.img}`}
+                alt={`Photo of ${place.placeName}`}
+                width={400}
+                height={300}
+            />
+            <div className="card-content">
+                <div className="card-content-container">
+                    <h2 className="card-title">{place.placeName}</h2>
+                    <p className="card-description">{place.info}</p>
+                </div>
+                <a href={url} target="_blank">
+                    <button className="card-button">Book Now</button>
+                </a>
+            </div>
+        </article>
+    );
 }
